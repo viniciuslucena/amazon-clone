3ca0972dc3ea5bdea5ad6d216231279cb80a167e
Vinicius <jviniiciuslucena@gmail.com>
2022-10-13 09:43:53 -0300

    feat: Banner finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,20 @@
 import Head from "next/head";
 
 import Header from "../components/Header";
+import Banner from "../components/Banner";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
 
       <Header />
+
+      <main className="max-w-screen-2xl mx-auto">
+        <Banner />
+      </main>
     </div>
   );
 }
