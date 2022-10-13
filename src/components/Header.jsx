04d5149ad8ec7add41921de4e5f099fae35e25e8
Vinicius <jviniiciuslucena@gmail.com>
2022-10-13 16:24:39 -0300

    feat: add route redirect in header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,11 @@ import {
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 export default function Header() {
   const { data } = useSession();
+  const router = useRouter();
 
   return (
     <header>
@@ -15,6 +17,7 @@ export default function Header() {
       <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
+            onClick={() => router.push("/")}
             src="https://links.papareact.com/f90"
             width={150}
             height={40}
@@ -42,7 +45,10 @@ export default function Header() {
             <p className="font-extrabold md:text-sm">& Orders</p>
           </div>
 
-          <div className="relative link flex items-center">
+          <div
+            className="relative link flex items-center"
+            onClick={() => router.push("/checkout")}
+          >
             <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
               0
             </span>
---
+++ src/pages/checkout.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "../components/Header";
+
+export default function Checkout() {
+  return (
+    <div className="bg-gray-100">
+      <Header />
+    </div>
+  );
+}
