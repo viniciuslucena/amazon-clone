f86a6879ede8e5438e1321d704b87fac6dd80970
Vinicius <jviniiciuslucena@gmail.com>
2022-10-13 12:44:01 -0300

    feat: next auth and firebase added to project

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC6haXzk7sGQNIS-Jr-_lEJ-gRIvLITeXo",
+  authDomain: "amzn-clone-8c5a9.firebaseapp.com",
+  projectId: "amzn-clone-8c5a9",
+  storageBucket: "amzn-clone-8c5a9.appspot.com",
+  messagingSenderId: "824522322312",
+  appId: "1:824522322312:web:73a4619840d796c58c4a3d"
+};
