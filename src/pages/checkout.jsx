a1f3598cdff9fe899889f0bde45c43d4d5ebc2ba
Vinicius <jviniiciuslucena@gmail.com>
2022-10-14 16:26:22 -0300

    feat: checkout structure

--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -5,6 +5,10 @@ export default function Checkout() {
   return (
     <div className="bg-gray-100">
       <Header />
+
+      <main className="max-w-screen-2xl mx-auto lg:flex">
+        <h1>Hello</h1>
+      </main>
     </div>
   );
 }
