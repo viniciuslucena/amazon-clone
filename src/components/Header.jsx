66719c6186798bcc0795e43af0bd04e33cd5168a
Vinicius <jviniiciuslucena@gmail.com>
2022-10-18 15:45:35 -0300

    refactor: products count on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,13 @@ import {
 } from "@heroicons/react/outline";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import { selectItems } from "../slices/basketSlice";
 
 export default function Header() {
   const { data } = useSession();
   const router = useRouter();
+  const items = useSelector(selectItems)
 
   return (
     <header>
@@ -50,7 +53,7 @@ export default function Header() {
             onClick={() => router.push("/checkout")}
           >
             <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
-              0
+              {items.length}
             </span>
             <ShoppingCartIcon className="h-10" />
             <p className="font-extrabold md:text-sm hidden md:inline mt-2">
