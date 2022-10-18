639b59424526072e5b7ef60c8767f08730a90553
Vinicius <jviniiciuslucena@gmail.com>
2022-10-18 15:45:03 -0300

    feat: added function to add product on basket

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import React, { useState } from "react";
 import { StarIcon } from "@heroicons/react/solid";
 import Currency from "react-currency-formatter";
+import { useDispatch } from "react-redux";
+import { addToBasket } from "../slices/basketSlice"
 
 const MAX_RATING = 5;
 const MIN_RATING = 1;
@@ -14,10 +16,25 @@ export default function Product({
   category,
   image,
 }) {
+  const dispatch = useDispatch()
   const [rating] = useState(
     Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
   );
   const [hasPrime] = useState(Math.random() < 0.5);
+
+  const addItemToBasket = () => {
+    const product = {
+      id,
+      title,
+      price,
+      description,
+      category,
+      image,
+    }
+
+    dispatch(addToBasket(product))
+  } 
+
   return (
     <div className="relative flex flex-col m-5 bg-white z-30 p-10">
       <p className="absolute top-2 right-2 text-sm italic text-gray-400">
@@ -53,7 +70,7 @@ export default function Product({
         </div>
       )}
 
-      <button className="mt-auto button">Add to Basket</button>
+      <button onClick={addItemToBasket} className="mt-auto button">Add to Basket</button>
     </div>
   );
 }
